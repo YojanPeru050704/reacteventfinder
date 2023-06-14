dc8fc3812c4f5c748433575494370c9828927297
aaguilar <alex.aguilar@sap.com>
2023-06-13 19:42:10 -0500

    react event finder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import React, {Fragment, useEffect, useState} from 'react'
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import SearchBar from './components/layout/SearchBar';
+import Events from './components/events/Events'
+import AddButon from './components/layout/AddButon';
+import AddEventModal from './components/events/AddEventModal'
+import EditEventModal from './components/events/EditEventModal';
+import ConfirmEventModal from './components/events/ConfirmEventModal'
+import RegisterUserModal from './components/auth/RegisterUserModal'
+import Login from './components/auth/Login';
+import store from './store'
+import {Provider} from 'react-redux'
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
-function App() {
+
+const  App = () => {
+
+   
+
+  useEffect(() => {
+      //Init Materialize javascript
+  
+      M.AutoInit();
+  }
+  )
+
+  console.log('validate');
+  console.log(store.getState());
+  //console.log(currentuserid);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <Router>
+    <Routes>        
+          <Route  path='/' element={
+              <Fragment>                
+                <RegisterUserModal></RegisterUserModal>       
+                <Login></Login> 
+              </Fragment>
+          }>
+          </Route>
+          <Route path='/events' element={
+              <Fragment>                
+                <SearchBar></SearchBar>  
+                <div className="container">                     
+                    <AddEventModal></AddEventModal>       
+                    <EditEventModal></EditEventModal>        
+                    <ConfirmEventModal></ConfirmEventModal>
+                    <Events></Events>        
+                    <AddButon></AddButon>        
+                </div>
+              </Fragment>
+          }>
+          </Route>        
+    </Routes>
+    </Router>
+    </Provider>
   );
 }
 
